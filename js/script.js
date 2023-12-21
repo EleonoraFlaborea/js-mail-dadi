78b882b10ecb2568e501cf78015abd57a96ec1c7
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2023-12-21 14:59:39 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*Ciao ragazzi,
+esercizio di oggi: Gioco dei dadi e Finta Login
+Nome repo: js-mail-dadi
+Oggi due esercizi al prezzo di uno, perchè "a Natale a Natale  si può fare di piùùùùù" :christmasparrot2::note:
+Potete svolgere entrambi gli esercizi in un unico file HTML e JS, se la cosa vi confonde, separateli, purchè siano sulla stessa repo.
+ESERCIZIO 1 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato
+
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+6. Il tuo codice ti sembra troppo complicato? probabilmente ti stai complicando la vita!*/
+
+console.log('JS OK');
